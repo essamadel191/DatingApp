3f65510fcd3d708ee8f1bd52f6ae90664b0d70bb
essamadel191 <essam.adel191@gmail.com>
2022-06-24 16:47:21 +0200

    Authentications Done

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,25 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
         {
             _context=context;
         }
+        [AllowAnonymous]
         [HttpGet]
         public async Task <ActionResult<IEnumerable<AppUser>>> GetUsers(){
             
            return await _context.Users.ToListAsync();
         }
+        [Authorize]
         [HttpGet("{id}")]
         public async Task <ActionResult<AppUser>> GetUser(int id){
             return await _context.Users.FindAsync(id);
