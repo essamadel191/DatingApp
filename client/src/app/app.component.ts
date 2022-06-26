75c5b2502fea2936ea1b100956a378127f9a721d
essamadel191 <essam.adel191@gmail.com>
2022-06-26 18:53:30 +0200

    Login & Registration (Angular)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient} from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
+
 
 
 @Component({
@@ -11,15 +14,15 @@ export class AppComponent implements OnInit{
   title = 'The Dating app';
   users: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private accountService:AccountService) { }
   ngOnInit(){
-    this.getUsers();  
+    this.setCurrentUser();
   }
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error)
-    })
+
+  setCurrentUser(){
+    const user:User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
+
 
 }
