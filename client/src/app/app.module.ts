f06cda6e2c42b5679147ca056783e408e67c58da
essamadel191 <essam.adel191@gmail.com>
2022-06-27 16:17:24 +0200

    Routing in Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,7 +9,12 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 
 @NgModule({
@@ -18,6 +23,11 @@ import { RegisterComponent } from './register/register.component';
     NavComponent,
     HomeComponent,
     RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent,
+    
 
   ],
   imports: [
@@ -26,7 +36,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,23 +14,24 @@ export class NavComponent implements OnInit {
   model: any={}
   
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService,private router:Router ,private toaster:ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login(){
     this.accountService.login(this.model).subscribe(response =>{
-      console.log(response);
+      this.router.navigateByUrl('/members');
       
     },error => {
       console.log(error);
+      this.toaster.error(error.error);
     });
   }
 
   logout(){
     this.accountService.logout();
- 
+    this.router.navigateByUrl('/');
   }
 
   
